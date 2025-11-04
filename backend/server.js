a0bcea81f8845b708fa8ffe4d80e8326eac8e467
Huzeyfa suleyman <shuzeyfa4@gmail.com>
2025-11-04 21:24:11 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ mongoose
   .catch((err) => console.log(err));
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests from your frontend
+  origin: "*", // Allow requests from your frontend
   credentials: true, // Allow credentials (cookies) to be sent
 };
 
